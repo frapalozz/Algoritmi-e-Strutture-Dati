3672dfd24592c3c3fd31a4aa9307610dc5167d1c
Francesco Palozzi <palozzifrancesco@libero.it>
2024-10-05 12:03:39 +0200

    Esercizio1 risolto

---
+++ src/it/unicam/cs/asdl2425/es1/EquazioneSecondoGradoModificabileConRisolutore.java
@@ -0,0 +1,189 @@
+/**
+ * 
+ */
+package it.unicam.cs.asdl2425.es1;
+
+/**
+ * Un oggetto di questa classe permette di rappresentare una equazione di
+ * secondo grado e di trovarne le soluzioni reali. I valori dei parametri
+ * dell'equazione possono cambiare nel tempo. All'inizio e ogni volta che viene
+ * cambiato un parametro la soluzione dell'equazione non esiste e deve essere
+ * calcolata con il metodo <code>solve()</code>. E' possibile sapere se al
+ * momento la soluzione dell'equazione esiste con il metodo
+ * <code>isSolved()</code>. Qualora la soluzione corrente non esista e si tenti
+ * di ottenerla verrà lanciata una eccezione.
+ * 
+ * @author Template: Luca Tesei, Implementation: Collettiva da Esercitazione a
+ *         Casa
+ *
+ */
+public class EquazioneSecondoGradoModificabileConRisolutore {
+    /*
+     * Costante piccola per il confronto di due numeri double
+     */
+    private static final double EPSILON = 1.0E-15;
+
+    private double a;
+
+    private double b;
+
+    private double c;
+
+    private boolean solved;
+
+    private SoluzioneEquazioneSecondoGrado lastSolution;
+
+    /**
+     * Costruisce una equazione di secondo grado modificabile. All'inizio
+     * l'equazione non è risolta.
+     * 
+     * 
+     * @param a
+     *              coefficiente del termine x^2, deve essere diverso da zero.
+     * @param b
+     *              coefficiente del termine x
+     * @param c
+     *              termine noto
+     * @throws IllegalArgumentException
+     *                                      se il parametro <code>a</code> è
+     *                                      zero
+     * 
+     */
+    public EquazioneSecondoGradoModificabileConRisolutore(double a, double b,
+            double c) {
+        // TODO implementare (FATTO)
+
+        if (Math.abs(a) < EPSILON) // controllo se uguale a zero
+            throw new IllegalArgumentException("L'equazione di secondo grado"
+                    + " non può avere coefficiente a uguale a zero");
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        solved = false;
+    }
+
+    /**
+     * @return il valore corrente del parametro a
+     */
+    public double getA() {
+        return a;
+    }
+
+    /**
+     * Cambia il parametro a di questa equazione. Dopo questa operazione
+     * l'equazione andrà risolta di nuovo.
+     * 
+     * @param a
+     *              il nuovo valore del parametro a
+     * @throws IllegalArgumentException
+     *                                      se il nuovo valore è zero
+     */
+    public void setA(double a) {
+        // TODO implementare (FATTO)
+
+        this.a = a;
+        solved = false;
+    }
+
+    /**
+     * @return il valore corrente del parametro b
+     */
+    public double getB() {
+        return b;
+    }
+
+    /**
+     * Cambia il parametro b di questa equazione. Dopo questa operazione
+     * l'equazione andrà risolta di nuovo.
+     * 
+     * @param b
+     *              il nuovo valore del parametro b
+     */
+    public void setB(double b) {
+        // TODO implementare (FATTO)
+
+        this.b = b;
+        solved = false;
+    }
+
+    /**
+     * @return il valore corrente del parametro c
+     */
+    public double getC() {
+        return c;
+    }
+
+    /**
+     * Cambia il parametro c di questa equazione. Dopo questa operazione
+     * l'equazione andrà risolta di nuovo.
+     * 
+     * @param c
+     *              il nuovo valore del parametro c
+     */
+    public void setC(double c) {
+        // TODO implementare (FATTO)
+
+        this.c = c;
+        this.solved = false;
+    }
+
+    /**
+     * Determina se l'equazione, nel suo stato corrente, è già stata risolta.
+     * 
+     * @return true se l'equazione è risolta, false altrimenti
+     */
+    public boolean isSolved() {
+        return solved;
+    }
+
+    /**
+     * Risolve l'equazione risultante dai parametri a, b e c correnti. Se
+     * l'equazione era già stata risolta con i parametri correnti non viene
+     * risolta di nuovo.
+     */
+    public void solve() {
+        // TODO implementare
+
+        double delta = b * b - 4 * a * c;
+        // delta == 0
+        if (Math.abs(delta) < EPSILON)
+            // ritorna la soluzione con due valori coincidenti
+            lastSolution =  new SoluzioneEquazioneSecondoGrado(new EquazioneSecondoGrado(a, b, c), (-b) / (2 * a));
+        // delta < 0
+        else if (delta < 0)
+            // ritorna la soluzione vuota
+            lastSolution = new SoluzioneEquazioneSecondoGrado(new EquazioneSecondoGrado(a, b, c));
+        // delta > 0
+        else {
+            double tmp = Math.sqrt(delta);
+            // ritorna le due soluzioni calcolate
+            lastSolution = new SoluzioneEquazioneSecondoGrado(new EquazioneSecondoGrado(a, b, c),
+                    (-b + tmp) / (2 * a),
+                    (-b - tmp) / (2 * a));   
+        }
+
+        solved = true;
+    }
+
+    /**
+     * Restituisce la soluzione dell'equazione risultante dai parametri
+     * correnti. L'equazione con i parametri correnti deve essere stata
+     * precedentemente risolta.
+     * 
+     * @return la soluzione
+     * @throws IllegalStateException
+     *                                   se l'equazione risulta non risolta,
+     *                                   all'inizio o dopo il cambiamento di
+     *                                   almeno uno dei parametri
+     */
+    public SoluzioneEquazioneSecondoGrado getSolution() {
+        // TODO implementare
+
+        if(solved) {
+            return lastSolution;
+        }
+        
+        throw new IllegalStateException("L'equazione non è ancora stata risolta!");
+    }
+
+}
---
+++ src/it/unicam/cs/asdl2425/es1/EquazioneSecondoGradoModificabileConRisolutoreTest.java
@@ -0,0 +1,156 @@
+/**
+ * 
+ */
+package it.unicam.cs.asdl2425.es1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Template: Luca Tesei, Implementation: Collettiva da Esercitazione a
+ *         Casa
+ *
+ */
+class EquazioneSecondoGradoModificabileConRisolutoreTest {
+    /*
+     * Costante piccola per il confronto di due numeri double
+     */
+    static final double EPSILON = 1.0E-15;
+
+    @Test
+    final void testEquazioneSecondoGradoModificabileConRisolutore() {
+        // controllo che il valore 0 su a lanci l'eccezione
+        assertThrows(IllegalArgumentException.class,
+                () -> new EquazioneSecondoGradoModificabileConRisolutore(0, 1,
+                        1));
+        // devo controllare che comunque nel caso normale il costruttore
+        // funziona
+        EquazioneSecondoGradoModificabileConRisolutore eq = new EquazioneSecondoGradoModificabileConRisolutore(
+                1, 1, 1);
+        // Controllo che all'inizio l'equazione non sia risolta
+        assertFalse(eq.isSolved());
+    }
+
+    @Test
+    final void testGetA() {
+        double x = 10;
+        EquazioneSecondoGradoModificabileConRisolutore e1 = new EquazioneSecondoGradoModificabileConRisolutore(
+                x, 1, 1);
+        // controllo che il valore restituito sia quello che ho messo
+        // all'interno
+        // dell'oggetto
+        assertTrue(x == e1.getA());
+        // in generale si dovrebbe usare assertTrue(Math.abs(x -
+        // e1.getA())<EPSILON) ma in
+        // questo caso il valore che testiamo non ha subito manipolazioni quindi
+        // la sua rappresentazione sarà la stessa di quella inserita nel
+        // costruttore senza errori di approssimazione
+
+    }
+
+    @Test
+    final void testSetA() {
+        // TODO implementare
+        double x = 5;
+        EquazioneSecondoGradoModificabileConRisolutore e1 = new EquazioneSecondoGradoModificabileConRisolutore(1, 1, 1);
+        e1.setA(x);
+
+        /*
+         * Controllo che il nuovo valore di a è quello che ho messo all'interno del setter,
+         * e controllo che l'equazione non è ancora risolta
+         */
+        assertTrue(Math.abs(e1.getA() - x)<EPSILON);
+        assertFalse(e1.isSolved());
+    }
+
+    @Test
+    final void testGetB() {
+        // TODO implementare
+        double x = 5;
+        EquazioneSecondoGradoModificabileConRisolutore e1 = new EquazioneSecondoGradoModificabileConRisolutore(1, x, 1);
+        /* controllo che il valore restituito sia quello che ho messo all'interno
+         * dell'oggetto
+         */
+        assertTrue(x == e1.getB());
+    }
+
+    @Test
+    final void testSetB() {
+        // TODO implementare
+        double x = 5;
+        EquazioneSecondoGradoModificabileConRisolutore e1 = new EquazioneSecondoGradoModificabileConRisolutore(1, 1, 1);
+        e1.setB(x);
+
+        /*
+         * Controllo che il nuovo valore di a è quello che ho messo all'interno del setter,
+         * e controllo che l'equazione non è ancora risolta
+         */
+        assertTrue(e1.getB() == x);
+        assertFalse(e1.isSolved());
+    }
+
+    @Test
+    final void testGetC() {
+        // TODO implementare
+        double x = 5;
+        EquazioneSecondoGradoModificabileConRisolutore e1 = new EquazioneSecondoGradoModificabileConRisolutore(1, 1, x);
+        /* controllo che il valore restituito sia quello che ho messo all'interno
+         * dell'oggetto
+         */
+        assertTrue(x == e1.getC());
+    }
+
+    @Test
+    final void testSetC() {
+        // TODO implementare
+        double x = 5;
+        EquazioneSecondoGradoModificabileConRisolutore e1 = new EquazioneSecondoGradoModificabileConRisolutore(1, 1, 1);
+        e1.setC(x);
+
+        /*
+         * Controllo che il nuovo valore di a è quello che ho messo all'interno del setter,
+         * e controllo che l'equazione non è ancora risolta
+         */
+        assertTrue(e1.getC() == x);
+        assertFalse(e1.isSolved());
+    }
+
+    @Test
+    final void testIsSolved() {
+        // TODO implementare
+        EquazioneSecondoGradoModificabileConRisolutore e1 = new EquazioneSecondoGradoModificabileConRisolutore(1, 1, 1);
+        // Controllo che l'equazione appena creata non è risolta
+        assertFalse(e1.isSolved());
+
+        e1.solve();
+        // Controllo che l'equazione è stata risolta dopo aver chiamato il metodo solve();
+        assertTrue(e1.isSolved());
+    }
+
+    @Test
+    final void testSolve() {
+        EquazioneSecondoGradoModificabileConRisolutore e3 = new EquazioneSecondoGradoModificabileConRisolutore(
+                1, 1, 3);
+        // controllo semplicemente che la chiamata a solve() non generi errori
+        e3.solve();
+        // i test con i valori delle soluzioni vanno fatti nel test del metodo
+        // getSolution()
+    }
+
+    @Test
+    final void testGetSolution() {
+        // TODO implementare
+        EquazioneSecondoGradoModificabileConRisolutore e4 = new EquazioneSecondoGradoModificabileConRisolutore(1, 1, 1);
+        assertThrows(IllegalStateException.class, () -> e4.getSolution());
+
+        e4.solve();
+        e4.getSolution();
+
+        double delta = e4.getB() * e4.getB() - 4 * e4.getA() * e4.getC();
+        if(Math.abs(delta) < EPSILON) assertTrue(e4.getSolution().isOneSolution());
+        else if(delta < 0) assertTrue(e4.getSolution().isEmptySolution());
+        else assertTrue(!e4.getSolution().isEmptySolution() && !e4.getSolution().isOneSolution());
+    }
+
+}
