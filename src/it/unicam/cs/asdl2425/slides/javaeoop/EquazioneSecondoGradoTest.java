bfe7c5f8a8ce726684a6b977f35c74a512d91170
Francesco Palozzi <palozzifrancesco@libero.it>
2024-11-22 11:28:33 +0100

    Suppress Warnings

--- src/it/unicam/cs/asdl2425/slides/javaeoop/EquazioneSecondoGradoTest.java
+++ src/it/unicam/cs/asdl2425/slides/javaeoop/EquazioneSecondoGradoTest.java
@@ -53,6 +53,7 @@ class EquazioneSecondoGradoTest {
         assertThrows(IllegalArgumentException.class,
                 () -> new EquazioneSecondoGrado(0, 1, 1));
         // costruzione senza eccezione
+        @SuppressWarnings("unused")
         EquazioneSecondoGrado e1 = new EquazioneSecondoGrado(1, 1, 1);
     }
 
