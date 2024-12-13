ec3f1ddc9f2aacb88ef731e9f965ae2dec767743
Francesco Palozzi <palozzifrancesco@libero.it>
2024-12-13 17:06:34 +0100

    Completato MaxHeap

--- src/it/unicam/cs/asdl2425/es10/MatrixMultiplicationSolverTest.java
+++ src/it/unicam/cs/asdl2425/es10/MatrixMultiplicationSolverTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Trim;
 
 class MatrixMultiplicationSolverTest {
 
--- src/it/unicam/cs/asdl2425/es10/MaxHeap.java
+++ src/it/unicam/cs/asdl2425/es10/MaxHeap.java
@@ -56,6 +56,14 @@ public class MaxHeap<E extends Comparable<E>> {
      */
     public MaxHeap(List<E> list) {
         // TODO implementare
+        if(list == null)
+            throw new NullPointerException("list null!");
+
+        // Creazione Max-Heap
+        heap = new ArrayList<>(list);
+        for(int i = heap.size()/2-1; i >= 0; i--){
+            heapify(i);
+        }
     }
 
     /**
@@ -69,6 +77,16 @@ public class MaxHeap<E extends Comparable<E>> {
      */
     public void insert(E el) {
         // TODO implementare
+        if(el == null)
+            throw new NullPointerException("el null!");
+
+        int i = heap.size();
+        heap.add(el);
+
+        while(i > 0 && heap.get(parentIndex(i)).compareTo(heap.get(i)) < 0){
+            scambia(i, parentIndex(i));
+            i = parentIndex(i);
+        }
     }
 
     /*
@@ -78,7 +96,7 @@ public class MaxHeap<E extends Comparable<E>> {
      */
     private int leftIndex(int i) {
         // TODO implementare
-        return -1;
+        return i*2 + 1;
     }
 
     /*
@@ -88,7 +106,7 @@ public class MaxHeap<E extends Comparable<E>> {
      */
     private int rightIndex(int i) {
         // TODO implementare
-        return -1;
+        return i*2 + 2;
     }
 
     /*
@@ -98,7 +116,7 @@ public class MaxHeap<E extends Comparable<E>> {
      */
     private int parentIndex(int i) {
         // TODO implementare
-        return -1;
+        return (i-1)/2;
     }
 
     /**
@@ -108,7 +126,10 @@ public class MaxHeap<E extends Comparable<E>> {
      */
     public E getMax() {
         // TODO implementare
-        return null;
+        if(heap.size() == 0)
+            return null;
+            
+        return heap.get(0);
     }
 
     /**
@@ -119,7 +140,19 @@ public class MaxHeap<E extends Comparable<E>> {
      */
     public E extractMax() {
         // TODO implementare
-        return null;
+        if(heap.size() == 0)
+            return null;
+
+        if(heap.size() == 1)
+            return heap.remove(0);
+        
+
+        scambia(0, heap.size()-1);
+        E max = heap.remove(heap.size()-1);
+
+        heapify(0);
+
+        return max;
     }
 
     /*
@@ -128,6 +161,37 @@ public class MaxHeap<E extends Comparable<E>> {
      */
     private void heapify(int i) {
         // TODO implementare
+        int left = leftIndex(i);
+        int right = rightIndex(i);
+        int massimo;
+
+        if(left < heap.size() && heap.get(left).compareTo(heap.get(i)) > 0)
+            massimo = left;
+        else
+            massimo = i;
+
+        if(right < heap.size() && heap.get(right).compareTo(heap.get(massimo)) > 0)
+            massimo = right;
+
+        if(massimo != i){
+            scambia(i, massimo);
+            heapify(massimo);
+        }
+    }
+
+    private void scambia(int a, int b){
+        if(a < b){
+            E itemA = heap.remove(a);
+            E itemB = heap.remove(b-1);
+            heap.add(a, itemB);
+            heap.add(b, itemA);
+        }
+        else {
+            E itemB = heap.remove(b);
+            E itemA = heap.remove(a-1);
+            heap.add(b, itemA);
+            heap.add(a, itemB);
+        }
     }
     
     /**
